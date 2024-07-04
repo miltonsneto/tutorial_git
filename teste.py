791d60925825ed43bcd509dbe247e316df7f68a3
Milton Neto <mneto@unimedcampinas.com.br>
2024-07-04 09:57:21 -0300

    Commit inicial

---
+++ teste.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário para inserir dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma dos dois números
+soma = num1 + num2
+
+# Exibe o resultado
+print("A soma de", num1, "e", num2, "é:", soma)
